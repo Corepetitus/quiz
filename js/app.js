01cd8aea221883c1e075c72c6e911513d4c9780b
mz <mindaugas.corepetitus@gmail.com>
2026-01-21 09:45:19 +0200

    more secure

--- js/app.js
+++ js/app.js
@@ -524,7 +524,7 @@ async function submitLead(e) {
     credentials: 'include'
   });
   const csrf_token = (await csrfResponse.json()).csrf_token;
-
+  alert('CSRF TOKEN:', csrf_token);
   const payload = {
     email: email,
     phone: phone,
